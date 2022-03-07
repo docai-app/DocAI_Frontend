c08b8b4a721175e0089e8a142a306b17b7798e45
Chonwai <chonwaiun@gmail.com>
2022-03-07 23:59:14 +0800

    Add: Added the Classification Page.

---
+++ pages/classification/ClassificationContainer.tsx
@@ -0,0 +1,11 @@
+import ClassificationView from './ClassificationView';
+
+function ClassificationContainer() {
+    return (
+        <>
+            <ClassificationView />
+        </>
+    );
+}
+
+export default ClassificationContainer;
---
+++ pages/classification/index.tsx
@@ -0,0 +1,5 @@
+import SimpleLayout from '../../components/layout/SimpleLayout';
+import ClassificationContainer from './ClassificationContainer';
+import withLayout from '../../components/hocs/withLayout';
+
+export default withLayout(ClassificationContainer, SimpleLayout);
