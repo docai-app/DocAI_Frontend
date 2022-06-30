72adab8234dd8015d873b15aff5c8d8ed216f586
Matthew Ngan <matthewngan3@gmail.com>
2022-06-30 17:18:07 +0800

    Add: Added document API class

---
+++ apis/Document.ts
@@ -0,0 +1,57 @@
+import { Method } from 'axios';
+
+// apis/Document.ts
+const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+interface requestHeaderProps {
+    baseURL: string | undefined;
+    url: string;
+    method: Method;
+}
+
+export default class Absence {
+    getAllDocuments() {
+        const requestHeader: requestHeaderProps = {
+            baseURL: baseURL,
+            url: `/documents`,
+            method: 'GET'
+        };
+        return requestHeader;
+    }
+
+    getDocumentById(id: string) {
+        const requestHeader: requestHeaderProps = {
+            baseURL: baseURL,
+            url: `/documents/${id}`,
+            method: 'GET'
+        };
+        return requestHeader;
+    }
+
+    getDocumentBtLabelId(id: string) {
+        const requestHeader: requestHeaderProps = {
+            baseURL: baseURL,
+            url: `/documents/labels/${id}`,
+            method: 'GET'
+        };
+        return requestHeader;
+    }
+
+    getAllUploadedDocuments() {
+        const requestHeader: requestHeaderProps = {
+            baseURL: baseURL,
+            url: `/documents/uploaded`,
+            method: 'GET'
+        };
+        return requestHeader;
+    }
+
+    getAndPredictLatestUploadedDocument() {
+        const requestHeader: requestHeaderProps = {
+            baseURL: baseURL,
+            url: `/documents/latest`,
+            method: 'GET'
+        };
+        return requestHeader;
+    }
+}
