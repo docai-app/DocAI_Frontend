ae8bfd1a3eda8beca0cf597096287099f9c61e05
Matthew Ngan <matthewngan3@gmail.com>
2022-07-18 20:13:00 +0800

    Add: Added login functionality Now request token when login And will save the token to localStorage

--- pages/login/LoginContainer.tsx
+++ pages/login/LoginContainer.tsx
@@ -1,4 +1,31 @@
+import useAxios from 'axios-hooks';
+import { FormEventHandler } from 'react';
+import Api from '../../apis';
 import LoginView from './LoginView';
+import { useRouter } from 'next/router';
+
+const apiSetting = new Api();
+
 export default function LoginContainer() {
-    return <LoginView />;
+    const router = useRouter();
+    const [{ data: signInData, loading: signInLoading, error: signInError }, signIn] = useAxios(
+        '',
+        { manual: true }
+    );
+    const handleSignIn: FormEventHandler<HTMLFormElement> = async (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        const email = formData.get('email') as string;
+        const password = formData.get('password') as string;
+        const res = await signIn(apiSetting.Authorization.signIn(email, password));
+        // if (res.data.success) {
+        if (res.headers.authorization) {
+            const token = res.headers.authorization;
+            localStorage.setItem('authorization', token);
+            router.push('/');
+        }
+    };
+    return (
+        <LoginView handleSignIn={handleSignIn} {...{ signInData, signInLoading, signInError }} />
+    );
 }
