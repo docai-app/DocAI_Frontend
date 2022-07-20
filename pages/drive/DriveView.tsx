18e0ace9afd8f1d54be97b7a752693e0226ed7dc
Matthew Ngan <matthewngan3@gmail.com>
2022-07-20 12:01:17 +0800

    Add: Added basic drive view

--- pages/drive/DriveView.tsx
+++ pages/drive/DriveView.tsx
@@ -1,5 +1,8 @@
-import { PlusIcon } from '@heroicons/react/outline';
+import { DocumentIcon, PhotographIcon, PlusIcon } from '@heroicons/react/outline';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 export default function DriveView() {
+    const router = useRouter();
     return (
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="py-4 flex flex-col gap-4">
@@ -10,20 +13,67 @@ export default function DriveView() {
                     </button>
                 </div>
                 <div className="py-3">
-                    <a className="hover:underline cursor-pointer text-lg">{`<< 返回`}</a>
+                    <a
+                        className="hover:underline cursor-pointer text-lg"
+                        onClick={() => {
+                            router.back();
+                        }}
+                    >{`<< 返回`}</a>
                 </div>
-                <div className="bg-white border shadow-md rounded-md">
-                    <table className="w-full divide-y">
-                        <thead>
+                <div className="bg-white shadow-md rounded-lg overflow-hidden ring-1 ring-black ring-opacity-5">
+                    <table className="w-full divide-y divide-gray-200">
+                        <thead className="bg-gray-50 shadow-sm">
                             <tr>
-                                <th className="px-12 py-3 w-5/12 text-left">名稱</th>
-                                <th className="px-12 py-3 w-4/12 text-left">修改日期</th>
-                                <th className="px-12 py-3 w-3/12 text-left">檔案大小</th>
+                                <th scope="col" className="px-2 py-3 w-1/12">
+                                    <DocumentIcon className="ml-auto h-6" />
+                                </th>
+                                <th scope="col" className="px-2 py-3 w-5/12 text-left">
+                                    名稱
+                                </th>
+                                <th scope="col" className="px-2 py-3 w-4/12 text-left">
+                                    修改日期
+                                </th>
+                                <th scope="col" className="px-2 py-3 w-2/12 text-left">
+                                    檔案大小
+                                </th>
                             </tr>
                         </thead>
-                        <tbody>
+                        <tbody className="divide-y divide-gray-100">
                             <tr>
-                                <td>Hello.pdf</td>
+                                <td scope="col" className="px-2 py-3">
+                                    <PhotographIcon className="ml-auto h-6" />
+                                </td>
+                                <td className="px-2 py-4 text-left">
+                                    <Link href="">
+                                        <a>Hello.jpeg</a>
+                                    </Link>
+                                </td>
+                                <td className="px-2 py-4 text-left">2022/7/20</td>
+                                <td className="px-2 py-4 text-left">5KB</td>
+                            </tr>
+                            <tr>
+                                <td scope="col" className="px-2 py-3">
+                                    <DocumentIcon className="ml-auto h-6" />
+                                </td>
+                                <td className="px-2 py-4 text-left">
+                                    <Link href="">
+                                        <a>Hello.jpeg</a>
+                                    </Link>
+                                </td>
+                                <td className="px-2 py-4 text-left">2022/7/20</td>
+                                <td className="px-2 py-4 text-left">5KB</td>
+                            </tr>
+                            <tr>
+                                <td scope="col" className="px-2 py-3">
+                                    <PhotographIcon className="ml-auto h-6" />
+                                </td>
+                                <td className="px-2 py-4 text-left">
+                                    <Link href="">
+                                        <a>Hello.jpeg</a>
+                                    </Link>
+                                </td>
+                                <td className="px-2 py-4 text-left">2022/7/20</td>
+                                <td className="px-2 py-4 text-left">5KB</td>
                             </tr>
                         </tbody>
                     </table>
