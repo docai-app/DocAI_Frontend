37ad51d538934ee90123b517ccbd15b15124d1e9
Matthew Ngan <matthewngan3@gmail.com>
2022-08-06 16:45:18 +0800

    Add: Added account button in HeadNav

--- pages/login/LoginContainer.tsx
+++ pages/login/LoginContainer.tsx
@@ -1,6 +1,5 @@
 import useAxios from 'axios-hooks';
-import axios from 'axios';
-import { FormEventHandler } from 'react';
+import { FormEventHandler, useEffect, useState } from 'react';
 import Api from '../../apis';
 import LoginView from './LoginView';
 import { useRouter } from 'next/router';
@@ -13,21 +12,34 @@ export default function LoginContainer() {
         '',
         { manual: true }
     );
+    useEffect(() => {
+        localStorage.removeItem('authorization');
+        localStorage.removeItem('email');
+        document.cookie = `authorization=null; expires=Thu, 01 Jan 1970 00:00:01 GMT`;
+    }, []);
     const handleSignIn: FormEventHandler<HTMLFormElement> = async (e) => {
         e.preventDefault();
         const formData = new FormData(e.currentTarget);
         const email = formData.get('email') as string;
         const password = formData.get('password') as string;
+        const remember = formData.get('remember');
         const res = await signIn(apiSetting.Authorization.signIn(email, password));
-        // if (res.data.success) {
-        if (res.headers.authorization) {
+        if (res.data.success) {
+            //if (res.headers.authorization) {
             const token = res.headers.authorization;
             localStorage.setItem('authorization', token);
-            const expiryDate = 'Fri, 31 Dec 9999 23:59:59 GMT'; // to be updated so that this can be dynamic
-            document.cookie = `authorization=${escape(token)}; expires=${expiryDate}`;
-            router.reload();
+            localStorage.setItem('email', email);
+            if (remember) {
+                const expiryDate = 'Fri, 31 Dec 9999 23:59:59 GMT'; // to be updated so that this can be dynamic
+                document.cookie = `authorization=${escape(token)}; expires=${expiryDate}`;
+            } else {
+                document.cookie = `authorization=${escape(token)}`;
+            }
+            if (router.pathname === 'login') router.push('/');
+            else router.reload();
         } else {
             localStorage.removeItem('authorization');
+            localStorage.removeItem('email');
             document.cookie = `authorization=null; expires=Thu, 01 Jan 1970 00:00:01 GMT`;
         }
     };
