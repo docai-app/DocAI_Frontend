2c343d8b844e6f7b34b39cf9f34edacb8e2e8f7f
Matthew Ngan <matthewngan3@gmail.com>
2022-12-01 10:35:40 +0800

    Update: Added document previewer Refactored code Previewer now opens document in a new tab

---
+++ components/common/Widget/DocumentPreview.tsx
@@ -0,0 +1,46 @@
+function DocumentPreview({ document }: { document: any }) {
+    return (
+        <div key={document.id} className="group relative">
+            <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+                {document.storage_url?.split(/[#?]/)[0].split('.').pop().trim() === 'pdf' ? (
+                    <object
+                        className="w-full h-full object-center object-cover lg:w-full lg:h-full flex justify-center items-center"
+                        type="application/pdf"
+                        data={document.storage_url + '#toolbar=0'}
+                        width="250"
+                        height="200"
+                    >
+                        <img
+                            src={
+                                'https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/PDF_file_icon.svg/833px-PDF_file_icon.svg.png'
+                            }
+                            alt="PDF file icon"
+                            className="w-1/2 h-1/2 object-contain object-center"
+                        />
+                    </object>
+                ) : (
+                    <img
+                        src={document.storage_url}
+                        alt={document.name}
+                        className="w-full h-full object-contain object-center lg:w-full lg:h-full"
+                    />
+                )}
+            </div>
+            <div className="mt-4 flex justify-between overflow-hidden">
+                <div>
+                    <h3 className="text-sm text-gray-700">
+                        <a href={document.storage_url} target="_blank" rel="noreferrer">
+                            <span aria-hidden="true" className="absolute inset-0" />
+                            <p>文件名稱：{document.name}</p>
+                            <p>
+                                上傳日期：
+                                {new Date(document.created_at).toISOString().split('T')[0]}
+                            </p>
+                        </a>
+                    </h3>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default DocumentPreview;
--- pages/classification/[tag]/TagView.tsx
+++ pages/classification/[tag]/TagView.tsx
@@ -2,6 +2,7 @@ import _get from 'lodash/get';
 import _map from 'lodash/map';
 import SingleActionModel from '../../../components/common/Widget/SingleActionModel';
 import { SearchIcon } from '@heroicons/react/outline';
+import DocumentPreview from '../../../components/common/Widget/DocumentPreview';
 
 interface LabelViewProps {
     documents: Array<any>;
@@ -39,53 +40,8 @@ export default function TagView(props: LabelViewProps) {
 
             <div className="px-16">
                 <div className="mt-8 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-                    {documents.map((document) => (
-                        <div key={document.id} className="group relative">
-                            <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
-                                {document.storage_url?.split(/[#?]/)[0].split('.').pop().trim() ===
-                                'pdf' ? (
-                                    <object
-                                        className="w-full h-full object-center object-cover lg:w-full lg:h-full flex justify-center items-center"
-                                        type="application/pdf"
-                                        data={document.storage_url + '#toolbar=0'}
-                                        width="250"
-                                        height="200"
-                                    >
-                                        <img
-                                            src={
-                                                'https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/PDF_file_icon.svg/833px-PDF_file_icon.svg.png'
-                                            }
-                                            alt="PDF file icon"
-                                            className="w-1/2 h-1/2 object-contain object-center"
-                                        />
-                                    </object>
-                                ) : (
-                                    <img
-                                        src={document.storage_url}
-                                        alt={document.name}
-                                        className="w-full h-full object-contain object-center lg:w-full lg:h-full"
-                                    />
-                                )}
-                            </div>
-                            <div className="mt-4 flex justify-between overflow-hidden">
-                                <div>
-                                    <h3 className="text-sm text-gray-700">
-                                        <a href={document.storage_url}>
-                                            <span aria-hidden="true" className="absolute inset-0" />
-                                            <p>文件名稱：{document.name}</p>
-                                            <p>
-                                                上傳日期：
-                                                {
-                                                    new Date(document.created_at)
-                                                        .toISOString()
-                                                        .split('T')[0]
-                                                }
-                                            </p>
-                                        </a>
-                                    </h3>
-                                </div>
-                            </div>
-                        </div>
+                    {documents.map((document, idx) => (
+                        <DocumentPreview document={document} key={idx} />
                     ))}
                 </div>
             </div>
