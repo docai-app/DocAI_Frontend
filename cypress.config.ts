3129fac2f166e5c504424c8e5007ca91638b71f3
Siimy-Laptop <qq1074306467@qq.com>
2023-08-09 18:21:48 +0900

    add: coverage report

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,18 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
     e2e: {
+        // baseUrl:'http://localhost:1234',
         setupNodeEvents(on, config) {
             // implement node event listeners here
+
+            // code coverage
+            require('@cypress/code-coverage/task')(on, config)
+            // include any other plugin code...
+      
+            // It's IMPORTANT to return the config object
+            // with any changed environment variables
+            return config
+
         }
     }
 });
--- cypress/e2e/3-login-page/login.cy.js
+++ cypress/e2e/3-login-page/login.cy.js
@@ -1,7 +1,12 @@
 /// <reference types="cypress" />
 
 describe('template spec', () => {
-    it('passes', () => {
-        cy.visit('https://example.cypress.io');
-    });
+    it('Login', () => {
+        cy.visit('https://chyb-dev.docai.net')
+        cy.get(':nth-child(1) > .mt-1').type('admin@chyb.com') // input username
+        cy.get(':nth-child(2) > .mt-1').focus().type('chyb123123') // input password
+        cy.get('.bg-indigo-600').click()
+    })
+
+
 });
