626513cab1cd07985ab0605e0bb6dbb83f670c89
Matthew Ngan <matthewngan3@gmail.com>
2023-08-14 15:44:28 +0800

    Add: Implemented Chabot

---
+++ pages/chatbot/[id]/index.tsx
@@ -0,0 +1,5 @@
+import SimpleLayout from '../../../components/layout/SimpleLayout';
+import ChatbotConversationContainer from './ChatbotConversationContainer';
+import withLayout from '../../../components/hocs/withLayout';
+
+export default withLayout(ChatbotConversationContainer, SimpleLayout);
