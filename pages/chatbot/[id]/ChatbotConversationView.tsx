174fa18697789ef1da2e8051b2243f0405000d64
Chonwai <chonwaiun@gmail.com>
2023-08-22 02:42:24 +0800

    Update: Try to hide the iframe.

--- pages/chatbot/[id]/ChatbotConversationView.tsx
+++ pages/chatbot/[id]/ChatbotConversationView.tsx
@@ -4,12 +4,12 @@ export default function ChatbotConversationView() {
     const router = useRouter();
     return (
         <>
-            {router.query?.id && (
+            {/* {router.query?.id && (
                 <iframe
                     src={`${process.env.NEXT_PUBLIC_CHATBOT_URL}${router.query.id as string}`}
                     className="h-full w-full"
                 />
-            )}
+            )} */}
         </>
     );
 }
