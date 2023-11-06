be9573c2087bde75b8d09206f9598ca5d0e1b59c
聪 <505810824@qq.com>
2023-11-06 18:24:37 +0800

    add home to execl and schem ui

--- pages/drive/DriveView.tsx
+++ pages/drive/DriveView.tsx
@@ -1,3 +1,4 @@
+import { Box, boxesIntersect, useSelectionContainer } from '@air/react-drag-to-select';
 import { Dialog, Menu, Transition } from '@headlessui/react';
 import {
     CheckCircleIcon,
@@ -34,8 +35,8 @@ import DeepUnderstandingModal from '../../components/feature/drive/DeepUnderstan
 import EditItems from '../../components/feature/drive/EditItems';
 import SearchLabelDocumentForm from '../../components/feature/drive/SearchLabelDocumentForm';
 import TableRow from '../../components/feature/drive/TableRow';
+import SelectDataSchemaModal from '../../components/feature/search/SelectDataSchemaModal';
 import EditLabel from '../../components/feature/setting/label/EditLabel';
-import { Box, boxesIntersect, useSelectionContainer } from '@air/react-drag-to-select';
 
 interface DriveViewProps {
     id: string | string[] | null | undefined;
@@ -90,15 +91,15 @@ export default function DriveView(props: DriveViewProps) {
         showAllItemsData = null,
         showAllItemsLoading = null,
         mode = 'view',
-        setMode = () => {},
+        setMode = () => { },
         target = [],
-        setTarget = () => {},
+        setTarget = () => { },
         dest = null,
-        setDest = () => {},
+        setDest = () => { },
         shareWith = [],
-        setShareWith = () => {},
-        handleShare = async () => {},
-        handleNewFolder = async () => {},
+        setShareWith = () => { },
+        handleShare = async () => { },
+        handleNewFolder = async () => { },
         countDocumentsByDateData = null,
         current,
         setCurrent,
@@ -135,6 +136,7 @@ export default function DriveView(props: DriveViewProps) {
     const [open, setOpen] = useState(false);
     const [openEditLabel, setOpenEditLabel] = useState(false);
     const [openDeepUnderstanding, setOpenDeepUnderstanding] = useState(false);
+    const [openSelectShema, setOpenSelectShema] = useState(false);
 
     const selectableItems = useRef<
         {
@@ -259,7 +261,7 @@ export default function DriveView(props: DriveViewProps) {
                     icon: ClockIcon,
                     amount:
                         countDocumentsByDateData?.documents_count -
-                            countDocumentsByDateData?.unconfirmed_count || 0
+                        countDocumentsByDateData?.unconfirmed_count || 0
                 },
                 {
                     name: '累計未分類的文檔',
@@ -286,7 +288,7 @@ export default function DriveView(props: DriveViewProps) {
                 allLabelsData={getAllLabelsData}
                 confirmDocumentFormik={confirmDocumentFormik}
                 isSubmit={true}
-                setTagName={(name: string) => {}}
+                setTagName={(name: string) => { }}
                 setOpenEditLabel={setOpenEditLabel}
             />
             <EditLabel
@@ -330,6 +332,10 @@ export default function DriveView(props: DriveViewProps) {
                 visibleDeepUnderstanding={documents_items?.length > 0 && folders_items?.length == 0}
                 deepUnderstanding={() => {
                     setOpenDeepUnderstanding(true);
+                }}
+                visibleToExecl={documents_items?.length > 0 && folders_items?.length == 0}
+                toExecl={() => {
+                    setOpenSelectShema(true)
                 }}
                 count={documents_items?.length + folders_items?.length}
             />
@@ -436,9 +442,8 @@ export default function DriveView(props: DriveViewProps) {
                                         <Menu.Item>
                                             {({ active }) => (
                                                 <button
-                                                    className={`${
-                                                        active ? 'bg-gray-100' : ''
-                                                    } p-2 rounded-md w-full text-left flex flex-row items-center`}
+                                                    className={`${active ? 'bg-gray-100' : ''
+                                                        } p-2 rounded-md w-full text-left flex flex-row items-center`}
                                                     onClick={() => {
                                                         setMode('newFolder');
                                                     }}
@@ -472,7 +477,7 @@ export default function DriveView(props: DriveViewProps) {
                         <div className="w-full relative" ref={tableBodyRef}>
                             <DragSelection />
                             {(allItemsData || allFoldersItemsData) &&
-                            [...(allItemsData || []), ...(allFoldersItemsData || [])].length !=
+                                [...(allItemsData || []), ...(allFoldersItemsData || [])].length !=
                                 0 ? (
                                 <InfiniteScroll
                                     dataLength={allItemsData?.length} //This is important field to render the next data
@@ -531,8 +536,8 @@ export default function DriveView(props: DriveViewProps) {
                                     {showAllItemsData?.success
                                         ? '沒有檔案'
                                         : showAllItemsLoading
-                                        ? '載入中...'
-                                        : showAllItemsData?.error || 'Error'}
+                                            ? '載入中...'
+                                            : showAllItemsData?.error || 'Error'}
                                 </div>
                             )}
                         </div>
@@ -561,10 +566,10 @@ export default function DriveView(props: DriveViewProps) {
                             </thead>
                             <tbody className="divide-y w-full divide-gray-100">
                                 {showAllItemsData?.folders &&
-                                showAllItemsData?.documents &&
-                                showAllItemsData?.success &&
-                                (showAllItemsData.folders.length > 0 ||
-                                    showAllItemsData.documents.length > 0) ? (
+                                    showAllItemsData?.documents &&
+                                    showAllItemsData?.success &&
+                                    (showAllItemsData.folders.length > 0 ||
+                                        showAllItemsData.documents.length > 0) ? (
                                     <>
                                         {showAllItemsData.folders.map((doc: any) => {
                                             return (
@@ -604,8 +609,8 @@ export default function DriveView(props: DriveViewProps) {
                                             {showAllItemsData?.success
                                                 ? '沒有檔案'
                                                 : showAllItemsLoading
-                                                ? '載入中...'
-                                                : showAllItemsData?.error || 'Error'}
+                                                    ? '載入中...'
+                                                    : showAllItemsData?.error || 'Error'}
                                         </td>
                                     </tr>
                                 )}
@@ -782,6 +787,12 @@ export default function DriveView(props: DriveViewProps) {
                     handleDeepUnderstanding(form_schema_id, needs_approval);
                     setOpenDeepUnderstanding(false);
                 }}
+            />
+
+            <SelectDataSchemaModal
+                open={openSelectShema}
+                setOpen={setOpenSelectShema}
+                document_ids={[documents_items]}
             />
         </>
     );
